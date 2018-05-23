e485903647d97608c5d15ecd5c80678599df7654
ivonne <ivonne.gattringer@gmail.com>
2018-05-23 22:36:16 +0200

    deleted one useless if

--- assignment/lottery.h
+++ assignment/lottery.h
@@ -12,5 +12,12 @@
  */
 #ifndef ___LOTTERY_H
 #define ___LOTTERY_H
-
+#define UUID_LEN   40
+#define TIP_SIZE   17
+#define MAX_LINE_LEN   (UUID_LEN + 1 + TIP_SIZE + 1)
+bool init_lottery(char* filename, char j);
+bool get_tip(int tip, int* array);
+bool set_drawing(int* array);
+int get_tip_result(int tip);
+int get_right_tips_count(int tip);
 #endif
